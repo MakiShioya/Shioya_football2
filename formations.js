54c652cee78bc6dd1913e607403746815b96e2ec
MakiShioya <makimakisioya@gmail.com>
2026-05-04 05:31:03 +0900

    Update formations.js

--- formations.js
+++ formations.js
@@ -3,8 +3,8 @@ const FORMATIONS = {
     "4-2-3-1": [
         { id: "GK", reqPos: "GK", top: 85, left: 50 },
         { id: "RSB", reqPos: "RSB", top: 65, left: 85 },
-        { id: "RCB", reqPos: "CB", top: 70, left: 65 }, // 右のCB（適性はCBを見る）
-        { id: "LCB", reqPos: "CB", top: 70, left: 35 }, // 左のCB（適性はCBを見る）
+        { id: "RCB", reqPos: "CB", top: 70, left: 65 }, 
+        { id: "LCB", reqPos: "CB", top: 70, left: 35 }, 
         { id: "LSB", reqPos: "LSB", top: 65, left: 15 },
         { id: "RDMF", reqPos: "DMF", top: 50, left: 65 },
         { id: "LDMF", reqPos: "DMF", top: 50, left: 35 },
@@ -13,7 +13,56 @@ const FORMATIONS = {
         { id: "LSH", reqPos: "LSH", top: 35, left: 15 },
         { id: "CF", reqPos: "CF", top: 15, left: 50 }
     ],
+    "4-4-2": [
+        { id: "GK", reqPos: "GK", top: 85, left: 50 },
+        { id: "RSB", reqPos: "RSB", top: 65, left: 85 },
+        { id: "RCB", reqPos: "CB", top: 70, left: 65 },
+        { id: "LCB", reqPos: "CB", top: 70, left: 35 },
+        { id: "LSB", reqPos: "LSB", top: 65, left: 15 },
+        { id: "RCMF", reqPos: "CMF", top: 45, left: 65 },
+        { id: "LCMF", reqPos: "CMF", top: 45, left: 35 },
+        { id: "RSH", reqPos: "RSH", top: 40, left: 85 },
+        { id: "LSH", reqPos: "LSH", top: 40, left: 15 },
+        { id: "RCF", reqPos: "CF", top: 20, left: 65 },
+        { id: "LCF", reqPos: "CF", top: 20, left: 35 }
+    ],
     "4-3-3": [
-        // 同じように11人分定義する...
+        { id: "GK", reqPos: "GK", top: 85, left: 50 },
+        { id: "RSB", reqPos: "RSB", top: 65, left: 85 },
+        { id: "RCB", reqPos: "CB", top: 70, left: 65 },
+        { id: "LCB", reqPos: "CB", top: 70, left: 35 },
+        { id: "LSB", reqPos: "LSB", top: 65, left: 15 },
+        { id: "DMF", reqPos: "DMF", top: 55, left: 50 },
+        { id: "RCMF", reqPos: "CMF", top: 40, left: 70 },
+        { id: "LCMF", reqPos: "CMF", top: 40, left: 30 },
+        { id: "RWG", reqPos: "RWG", top: 25, left: 85 },
+        { id: "LWG", reqPos: "LWG", top: 25, left: 15 },
+        { id: "CF", reqPos: "CF", top: 15, left: 50 }
+    ],
+    "3-5-2": [
+        { id: "GK", reqPos: "GK", top: 85, left: 50 },
+        { id: "RCB", reqPos: "CB", top: 70, left: 75 },
+        { id: "CB", reqPos: "CB", top: 70, left: 50 },
+        { id: "LCB", reqPos: "CB", top: 70, left: 25 },
+        { id: "RSH", reqPos: "RSH", top: 45, left: 85 },
+        { id: "LSH", reqPos: "LSH", top: 45, left: 15 },
+        { id: "DMF", reqPos: "DMF", top: 55, left: 50 },
+        { id: "RCMF", reqPos: "CMF", top: 40, left: 70 },
+        { id: "LCMF", reqPos: "CMF", top: 40, left: 30 },
+        { id: "RCF", reqPos: "CF", top: 20, left: 65 },
+        { id: "LCF", reqPos: "CF", top: 20, left: 35 }
+    ],
+    "3-4-2-1": [
+        { id: "GK", reqPos: "GK", top: 85, left: 50 },
+        { id: "RCB", reqPos: "CB", top: 70, left: 75 },
+        { id: "CB", reqPos: "CB", top: 70, left: 50 },
+        { id: "LCB", reqPos: "CB", top: 70, left: 25 },
+        { id: "RSH", reqPos: "RSH", top: 45, left: 85 },
+        { id: "LSH", reqPos: "LSH", top: 45, left: 15 },
+        { id: "RDMF", reqPos: "DMF", top: 50, left: 65 },
+        { id: "LDMF", reqPos: "DMF", top: 50, left: 35 },
+        { id: "ROMF", reqPos: "OMF", top: 30, left: 65 },
+        { id: "LOMF", reqPos: "OMF", top: 30, left: 35 },
+        { id: "CF", reqPos: "CF", top: 15, left: 50 }
     ]
 };
